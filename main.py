eb399628d90ecea8b8753151c8bb572a47d0d6b4
Dash Eclipse <dashezup@disroot.org>
2021-03-08 13:35:28 +0000

    Add main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+from os import environ
+# import logging
+from pyrogram import Client, idle
+
+api_id = int(environ["API_ID"])
+api_hash = environ["API_HASH"]
+session_name = environ["SESSION_NAME"]
+
+plugins = dict(
+    root="plugins",
+    include=[
+        "vc.player",
+        "ping"
+    ]
+)
+
+app = Client(session_name, api_id, api_hash, plugins=plugins)
+# logging.basicConfig(level=logging.INFO)
+app.start()
+print('>>> USERBOT STARTED')
+idle()
+app.stop()
+print('\n>>> USERBOT STOPPED')
