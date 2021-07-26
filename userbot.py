a3066891fdc76c97b72fe6f7a612a95cbd53c0fe
khayal-0508 <80122272+khayal-0508@users.noreply.github.com>
2021-07-27 03:49:40 +0400

    Update userbot.py

--- userbot.py
+++ userbot.py
@@ -1,25 +1,18 @@
-"""
-tgvc-userbot, Telegram Voice Chat Userbot
-Copyright (C) 2021  Dash Eclipse
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-"""
-# import logging
 from pyrogram import Client, idle
 
-app = Client("tgvc")
-# logging.basicConfig(level=logging.INFO)
+api_id = 7097917
+api_hash = "f48294d395134512a2963d4315d21296"
+
+plugins = dict(
+    root="plugins",
+    include=[
+        "vc.player",
+        "ping",
+        "sysinfo"
+    ]
+)
+
+app = Client("tgvc", api_id, api_hash, plugins=plugins)
 app.start()
 print('>>> USERBOT STARTED')
 idle()
